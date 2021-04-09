7232223419a0970c039d4e91e936af2156861092
omanimaran <omanimaran@identiv.com>
2021-04-09 09:04:54 +0530

    Json number type fix

--- src/main/java/org/json/JSONObject.java
+++ src/main/java/org/json/JSONObject.java
@@ -255,15 +255,16 @@ public class JSONObject {
                         map.put("null", nValue);
                     }
                     return;
-                case 1 :
-                case 2:
-                case 3:
-                case 4:
-                case 5:
-                case 6:
-                case 7:
-                case 8:
-                case 9:
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
                 case '-':
                   x.back();
                   Object num = x.nextValue();
